428d8cf46f7d33eea237e0b483ad996cecba8915
Sapaev Shukur <shukur_06@mail.ru>
2020-09-04 03:03:31 +0500

    [+] Home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-link to="/create">Create</router-link>
+    <hr />
+    <div class="list-group">
+      <router-link
+        :to="{ name: 'Read', params: { id: contact.id }}"
+        class="list-group-item list-group-item-action"
+        v-for="contact in contacts"
+        :key="contact.id"
+      >
+        {{ contact.name }}
+        <small class="float-right">view more</small>
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  computed: {
+    count() {
+      return this.$store.getters.getLength;
+    },
+    contacts() {
+      return this.$store.state.contacts;
+    }
+  },
+};
 </script>
