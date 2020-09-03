23e5249c21dff21c0bd1d7019cad494115c4a704
Sapaev Shukur <shukur_06@mail.ru>
2020-09-04 03:35:04 +0500

    [BF] BugFix with reactivity of data

--- src/views/Update.vue
+++ src/views/Update.vue
@@ -3,13 +3,13 @@
     <h6>
       <router-link :to="{name:'Home'}">Back to home</router-link>
     </h6>
-    <div class="contact" v-if="contact">
+    <div class="contact" v-if="updateContact">
       <h3 class="mb-4">Update contact</h3>
 
       <form @submit.prevent="save()">
         <div class="form-group">
           <label for="name mb-5">Name</label>
-          <input type="text" class="form-control" id="name" required v-model="contact.name" />
+          <input type="text" class="form-control" id="name" required v-model="updateContact.name" />
         </div>
         <div class="form-group mb-5 mt-5">
           <label for="phone" style="width:100%">
@@ -18,14 +18,14 @@
               <div class="btn btn-sm btn-primary" @click="add('phones')">+</div>
             </small>
           </label>
-          <div class="d-flex mb-1" v-for="(phone,index) in contact.phones" :key="index">
+          <div class="d-flex mb-1" v-for="(phone,index) in updateContact.phones" :key="index">
             <input
               type="text"
               class="form-control"
               :placeholder="`${index+1} phone`"
               id="phone"
               required
-              v-model="contact.phones[index]"
+              v-model="updateContact.phones[index]"
             />
             <div class="d-flex ml-3">
               <div class="m-auto btn btn-sm btn-danger" @click="remove('phones',index)">x</div>
@@ -39,14 +39,14 @@
               <div class="btn btn-sm btn-primary" @click="add('addresses')">+</div>
             </small>
           </label>
-          <div class="d-flex mb-1" v-for="(address,index) in contact.addresses" :key="index">
+          <div class="d-flex mb-1" v-for="(address,index) in updateContact.addresses" :key="index">
             <input
               type="text"
               class="form-control"
               :placeholder="`${index+1} address`"
               id="address"
               required
-              v-model="contact.addresses[index]"
+              v-model="updateContact.addresses[index]"
             />
             <div class="d-flex ml-3">
               <div class="m-auto btn btn-sm btn-danger" @click="remove('addresses',index)">x</div>
@@ -60,14 +60,14 @@
               <div class="btn btn-sm btn-primary" @click="add('emails')">+</div>
             </small>
           </label>
-          <div class="d-flex mb-1" v-for="(email,index) in contact.emails" :key="index">
+          <div class="d-flex mb-1" v-for="(email,index) in updateContact.emails" :key="index">
             <input
               type="email"
               class="form-control"
               :placeholder="`${index+1} email`"
               id="email"
               required
-              v-model="contact.emails[index]"
+              v-model="updateContact.emails[index]"
             />
             <div class="d-flex ml-3">
               <div class="m-auto btn btn-sm btn-danger" @click="remove('emails',index)">x</div>
@@ -82,10 +82,16 @@
 
 <script>
 export default {
+  data() {
+    return {
+      updateContact: null
+    };
+  },
   mounted() {
     if (!this.contact) {
       this.$router.push({ name: "About" });
     }
+    this.updateContact = JSON.parse(JSON.stringify(this.contact));
   },
   computed: {
     contact() {
@@ -94,7 +100,7 @@ export default {
   },
   methods: {
     save() {
-      this.$store.dispatch("updateContact", this.contact);
+      this.$store.dispatch("updateContact", this.updateContact);
       this.$router.push({
         name: "Read",
         params: { id: Number(this.$route.params.id) }
@@ -102,19 +108,19 @@ export default {
     },
     add(type) {
       if (
-        this.contact[type].length &&
-        this.contact[type][this.contact[type].length - 1] == ""
+        this.updateContact[type].length &&
+        this.updateContact[type][this.updateContact[type].length - 1] == ""
       ) {
         return;
       }
-      this.contact[type].push("");
+      this.updateContact[type].push("");
     },
     remove(type, index) {
-      if (this.contact[type].length === 1) {
-        this.contact[type] = [""];
+      if (this.updateContact[type].length === 1) {
+        this.updateContact[type] = [""];
         return;
       }
-      this.contact[type].splice(index, 1);
+      this.updateContact[type].splice(index, 1);
     }
   }
 };
