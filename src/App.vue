128a2fd64eada3e3cae2ede0f04e200ecbc4a2aa
Sapaev Shukur <shukur_06@mail.ru>
2020-09-04 03:26:58 +0500

    [+] Signature :alien:

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
-  <div id="app">
-    <div class="container mt-5">
+  <div id="app" style="position:relative;" class="pb-5">
+    <div class="container mt-5 mb-5">
       <div class="row">
         <div class="col-lg-6 m-auto">
           <router-view />
         </div>
       </div>
     </div>
+    <h6 class="text-muted" style="position:fixed; bottom:0; right:10px;">Sapaev Shukur</h6>
   </div>
 </template>
 
